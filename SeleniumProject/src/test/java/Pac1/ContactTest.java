9d02983741ca22b765500210786dec39c02063c2
Vandhana963 <ramapuramvandhana.248@gmail.com>
2026-09-22 17:04:52 +0530

    First Commit

---
+++ SeleniumProject/src/test/java/Pac1/ContactTest.java
@@ -0,0 +1,29 @@
+package Pac1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest
+{
+	@Test
+	public void createContactTest()
+	{
+		String URL=System.getProperty("url");
+		String BROWSER=System.getProperty("username");
+		String USERNAME=System.getProperty("username");
+		String PASSWORD=System.getProperty("password");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		System.out.println("execute createContactTest");
+
+	}
+	
+	@Test
+	public void modifyContactTest()
+	{
+		System.out.println("execute modifyContactTest");
+	}
+
+}
