343feab077db884d8785d49102b1f67296cef64e
Vandhana963 <ramapuramvandhana.248@gmail.com>
2026-09-22 18:00:02 +0530

    edited data in contacttest

--- SeleniumProject/src/test/java/Pac1/ContactTest.java
+++ SeleniumProject/src/test/java/Pac1/ContactTest.java
@@ -29,7 +29,7 @@ public class ContactTest
 	@Test
 	public void deleteContactTest()
 	{
-		System.out.println("execute modifyContactTest");
+		System.out.println("execute deleteContactTest");
 	}
 
 }
